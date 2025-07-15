a1eaa520734a6e3a6e19a52794734a8f8989e6d0
Azra Javed <azraj063@gmail.com>
2025-07-15 18:28:21 +0500

    create featued product frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Layout/Header.jsx";
 import Hero from "../components/Route/Hero.jsx";
 import Categories from "../components/Route/Categories.jsx";
 import BestDeals from "../components/Route/BestDeals.jsx";
-
+import FeaturedProduct from "../components/Route/FeaturedProducts/FreaturedProduct.jsx";
 const HomePage = () => {
   return (
     <>
@@ -10,6 +10,7 @@ const HomePage = () => {
       <Hero />
       <Categories />
       <BestDeals />
+      <FeaturedProduct />
     </>
   );
 };

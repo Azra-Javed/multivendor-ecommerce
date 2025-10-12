ac1e58d56f407bf166eb6d247b7cec87e5e056b4
Azra Javed <azraj063@gmail.com>
2025-10-12 19:25:37 +0500

    fix the cart design

--- frontend/src/components/payment/CartData.jsx
+++ frontend/src/components/payment/CartData.jsx
@@ -1,21 +1,13 @@
 import styles from "../../styles/style";
 
-const CartData = ({
-  handleSubmit,
-  totalPrice,
-  shipping,
-  subTotalPrice,
-  couponCode,
-  setCouponCode,
-  discountPercentage,
-}) => {
+const CartData = ({ orderData }) => {
   return (
     <div className="w-full  bg-white shadow-xl  rounded-md p-5 pb-8">
       {/* Subtotal */}
       <div className="flex justify-between">
         <h3 className="text-[16px] font-[400] text-[#2D6A4F]">Subtotal:</h3>
         <h5 className="text-[18px] font-[600] text-[#2D6A4F]">
-          ${subTotalPrice.toFixed(2)}
+          ${orderData?.subTotalPrice?.toFixed(2)}
         </h5>
       </div>
 
@@ -24,7 +16,7 @@ const CartData = ({
       <div className="flex justify-between">
         <h3 className="text-[16px] font-[400] text-[#2D6A4F]">Shipping:</h3>
         <h5 className="text-[18px] font-[600] text-[#2D6A4F]">
-          ${shipping.toFixed(2)}
+          {(orderData?.shipping ?? 0).toFixed(2)}
         </h5>
       </div>
 
@@ -33,31 +25,14 @@ const CartData = ({
       <div className="flex justify-between border-b border-[#2D6A4F] pb-3">
         <h3 className="text-[16px] font-[400] text-[#2D6A4F]">Discount:</h3>
         <h5 className="text-[18px] font-[600] text-[#2D6A4F]">
-          - {discountPercentage ? "$" + discountPercentage : "0"}
+          - {orderData?.discountPrice ? "$" + orderData?.discountPrice : "-"}
         </h5>
       </div>
 
       {/* Total */}
       <div className="text-[18px] font-[600] text-end pt-3 text-[#2D6A4F]">
-        ${totalPrice.toFixed(2)}
+        {orderData?.totalPrice?.toFixed(2)}
       </div>
-
-      {/* Coupon form */}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className={`${styles.input} h-[40px] pl-2 mt-5`}
-          placeholder="Coupon code"
-          required
-          value={couponCode}
-          onChange={(e) => setCouponCode(e.target.value)}
-        />
-        <input
-          type="submit"
-          value="Apply code"
-          className="w-full h-[40px] bg-[#2D6A4F] text-white rounded-[3px] mt-5 cursor-pointer hover:bg-[#1B4332] transition"
-        />
-      </form>
     </div>
   );
 };
